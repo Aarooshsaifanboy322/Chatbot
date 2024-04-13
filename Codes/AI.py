0e7c0bb5c82fad8d67833a7e598c71c94f28f05a
AarooshRBLX <164038746+Aarooshsaifanboy322@users.noreply.github.com>
2024-04-13 18:52:51 +0600

    AI data Exporter

---
+++ Codes/AI.py
@@ -0,0 +1,12 @@
+# DANGEROUS FILE, DO NOT EDIT
+import time
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+# SOME IMPORTS MIGHT BE UNUSED
+
+plt.title('AI data exported')
+X = np.array([6, 7.25])
+Y = np.array([4.75, 0])
+plt.plot(X, Y)
+plt.show()
